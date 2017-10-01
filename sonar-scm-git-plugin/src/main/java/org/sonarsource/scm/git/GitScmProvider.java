4f7bf1d1b7bf03be235ff30c8d948cbfb5288a0c
redvulture <blackshark37@gmail.com>
2017-09-30 22:08:25 -0400

    adding directories and files for sonarcube

---
+++ sonar-scm-git-plugin/src/main/java/org/sonarsource/scm/git/GitScmProvider.java
@@ -0,0 +1,134 @@
+/*
+ * SonarQube :: Plugins :: SCM :: Git
+ * Copyright (C) 2014-2016 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.scm.git;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Set;
+import java.util.stream.Collectors;
+import javax.annotation.Nullable;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.lib.ObjectReader;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.RepositoryBuilder;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.revwalk.filter.RevFilter;
+import org.eclipse.jgit.treewalk.AbstractTreeIterator;
+import org.eclipse.jgit.treewalk.CanonicalTreeParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sonar.api.batch.scm.BlameCommand;
+import org.sonar.api.batch.scm.ScmProvider;
+import org.sonar.api.utils.MessageException;
+
+public class GitScmProvider extends ScmProvider {
+
+  private static final Logger LOG = LoggerFactory.getLogger(GitScmProvider.class);
+
+  private final JGitBlameCommand jgitBlameCommand;
+
+  public GitScmProvider(JGitBlameCommand jgitBlameCommand) {
+    this.jgitBlameCommand = jgitBlameCommand;
+  }
+
+  @Override
+  public String key() {
+    return "git";
+  }
+
+  @Override
+  public boolean supports(File baseDir) {
+    RepositoryBuilder builder = new RepositoryBuilder().findGitDir(baseDir);
+    return builder.getGitDir() != null;
+  }
+
+  @Override
+  public BlameCommand blameCommand() {
+    return this.jgitBlameCommand;
+  }
+
+  @Nullable
+  @Override
+  public Set<Path> branchChangedFiles(String targetBranchName, Path rootBaseDir) {
+    try (Repository repo = buildRepo(rootBaseDir)) {
+      Ref targetRef = repo.exactRef("refs/heads/" + targetBranchName);
+      if (targetRef == null) {
+        LOG.warn("Could not find ref: {}", targetBranchName);
+        return null;
+      }
+
+      try (Git git = newGit(repo)) {
+        return git.diff().setShowNameAndStatusOnly(true).setOldTree(prepareTreeParser(repo, targetRef)).call().stream()
+          .filter(diffEntry -> diffEntry.getChangeType() == DiffEntry.ChangeType.ADD || diffEntry.getChangeType() == DiffEntry.ChangeType.MODIFY)
+          .map(diffEntry -> repo.getWorkTree().toPath().resolve(diffEntry.getNewPath()))
+          .collect(Collectors.toSet());
+      }
+    } catch (IOException | GitAPIException e) {
+      LOG.warn(e.getMessage(), e);
+    }
+    return null;
+  }
+
+  private AbstractTreeIterator prepareTreeParser(Repository repo, Ref targetRef) throws IOException {
+    try (RevWalk walk = newRevWalk(repo)) {
+      walk.markStart(walk.parseCommit(targetRef.getObjectId()));
+      walk.markStart(walk.parseCommit(repo.exactRef("HEAD").getObjectId()));
+      walk.setRevFilter(RevFilter.MERGE_BASE);
+      RevCommit base = walk.parseCommit(walk.next());
+
+      CanonicalTreeParser treeParser = new CanonicalTreeParser();
+      try (ObjectReader objectReader = repo.newObjectReader()) {
+        treeParser.reset(objectReader, base.getTree());
+      }
+
+      walk.dispose();
+
+      return treeParser;
+    }
+  }
+
+  Git newGit(Repository repo) {
+    return new Git(repo);
+  }
+
+  RevWalk newRevWalk(Repository repo) {
+    return new RevWalk(repo);
+  }
+
+  Repository buildRepo(Path basedir) throws IOException {
+    return getVerifiedRepositoryBuilder(basedir).build();
+  }
+
+  static RepositoryBuilder getVerifiedRepositoryBuilder(Path basedir) {
+    RepositoryBuilder builder = new RepositoryBuilder()
+      .findGitDir(basedir.toFile())
+      .setMustExist(true);
+
+    if (builder.getGitDir() == null) {
+      throw MessageException.of("Not inside a Git work tree: " + basedir);
+    }
+    return builder;
+  }
+}
---
+++ sonar-scm-git-plugin/src/test/java/org/sonarsource/scm/git/GitScmProviderTest.java
@@ -0,0 +1,275 @@
+/*
+ * SonarQube :: Plugins :: SCM :: Git
+ * Copyright (C) 2014-2016 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.scm.git;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Random;
+import org.eclipse.jgit.api.DiffCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.TemporaryFolder;
+import org.sonar.api.scan.filesystem.PathResolver;
+import org.sonar.api.utils.MessageException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.sonarsource.scm.git.JGitBlameCommandTest.javaUnzip;
+
+public class GitScmProviderTest {
+
+  @Rule
+  public TemporaryFolder temp = new TemporaryFolder();
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  private static final Random random = new Random();
+
+  private Path worktree;
+  private Repository repo;
+  private Git git;
+
+  @Before
+  public void before() throws IOException, GitAPIException {
+    worktree = temp.newFolder().toPath();
+    repo = FileRepositoryBuilder.create(worktree.resolve(".git").toFile());
+    repo.create();
+
+    git = new Git(repo);
+
+    createAndCommitFile(worktree, "file-in-first-commit.xoo");
+  }
+
+  @Test
+  public void sanityCheck() {
+    assertThat(new GitScmProvider(mock(JGitBlameCommand.class)).key()).isEqualTo("git");
+  }
+
+  @Test
+  public void returnImplem() {
+    JGitBlameCommand jblameCommand = new JGitBlameCommand(new PathResolver());
+    GitScmProvider gitScmProvider = new GitScmProvider(jblameCommand);
+
+    assertThat(gitScmProvider.blameCommand()).isEqualTo(jblameCommand);
+  }
+
+  @Test
+  public void testAutodetection() throws IOException {
+    File baseDirEmpty = temp.newFolder();
+    assertThat(new GitScmProvider(mock(JGitBlameCommand.class)).supports(baseDirEmpty)).isFalse();
+
+    File projectDir = temp.newFolder();
+    javaUnzip(new File("test-repos/dummy-git.zip"), projectDir);
+    File baseDir = new File(projectDir, "dummy-git");
+    assertThat(newScmProvider().supports(baseDir)).isTrue();
+  }
+
+  private static JGitBlameCommand mockCommand() {
+    return mock(JGitBlameCommand.class);
+  }
+
+  @Test
+  public void branchChangedFiles_from_diverged() throws IOException, GitAPIException {
+    createAndCommitFile(worktree, "file-m1.xoo");
+    createAndCommitFile(worktree, "file-m2.xoo");
+    createAndCommitFile(worktree, "file-m3.xoo");
+    ObjectId forkPoint = git.getRepository().exactRef("HEAD").getObjectId();
+
+    appendToAndCommitFile(worktree, "file-m3.xoo");
+    createAndCommitFile(worktree, "file-m4.xoo");
+
+    git.branchCreate().setName("b1").setStartPoint(forkPoint.getName()).call();
+    git.checkout().setName("b1").call();
+    createAndCommitFile(worktree, "file-b1.xoo");
+    appendToAndCommitFile(worktree, "file-m1.xoo");
+    deleteAndCommitFile("file-m2.xoo");
+
+    assertThat(newScmProvider().branchChangedFiles("master", worktree))
+      .containsExactlyInAnyOrder(
+        worktree.resolve("file-b1.xoo"),
+        worktree.resolve("file-m1.xoo"));
+  }
+
+  @Test
+  public void branchChangedFiles_from_merged_and_diverged() throws IOException, GitAPIException {
+    createAndCommitFile(worktree, "file-m1.xoo");
+    createAndCommitFile(worktree, "file-m2.xoo");
+    ObjectId forkPoint = git.getRepository().exactRef("HEAD").getObjectId();
+
+    createAndCommitFile(worktree, "file-m3.xoo");
+    ObjectId mergePoint = git.getRepository().exactRef("HEAD").getObjectId();
+
+    appendToAndCommitFile(worktree, "file-m3.xoo");
+    createAndCommitFile(worktree, "file-m4.xoo");
+
+    git.branchCreate().setName("b1").setStartPoint(forkPoint.getName()).call();
+    git.checkout().setName("b1").call();
+    createAndCommitFile(worktree, "file-b1.xoo");
+    appendToAndCommitFile(worktree, "file-m1.xoo");
+    deleteAndCommitFile("file-m2.xoo");
+
+    git.merge().include(mergePoint).call();
+    createAndCommitFile(worktree, "file-b2.xoo");
+
+    assertThat(newScmProvider().branchChangedFiles("master", worktree))
+      .containsExactlyInAnyOrder(
+        worktree.resolve("file-m1.xoo"),
+        worktree.resolve("file-b1.xoo"),
+        worktree.resolve("file-b2.xoo"));
+  }
+
+  @Test
+  public void branchChangedFiles_when_git_work_tree_is_above_project_basedir() throws IOException, GitAPIException {
+    Path projectDir = worktree.resolve("project");
+    Files.createDirectory(projectDir);
+
+    git.branchCreate().setName("b1").call();
+    git.checkout().setName("b1").call();
+    createAndCommitFile(projectDir, "file-b1");
+
+    assertThat(newScmProvider().branchChangedFiles("master", projectDir))
+      .containsOnly(projectDir.resolve("file-b1"));
+  }
+
+  @Test
+  public void branchChangedFiles_should_return_null_when_branch_nonexistent() {
+    assertThat(newScmProvider().branchChangedFiles("nonexistent", worktree)).isNull();
+  }
+
+  @Test
+  public void branchChangedFiles_should_throw_when_repo_nonexistent() throws IOException {
+    thrown.expect(MessageException.class);
+    thrown.expectMessage("Not inside a Git work tree: ");
+    newScmProvider().branchChangedFiles("master", temp.newFolder().toPath());
+  }
+
+  @Test
+  public void branchChangedFiles_should_throw_when_dir_nonexistent() throws IOException {
+    thrown.expect(MessageException.class);
+    thrown.expectMessage("Not inside a Git work tree: ");
+    newScmProvider().branchChangedFiles("master", temp.getRoot().toPath().resolve("nonexistent"));
+  }
+
+  @Test
+  public void branchChangedFiles_should_return_null_on_io_errors_of_repo_builder() throws IOException {
+    GitScmProvider provider = new GitScmProvider(mockCommand()) {
+      @Override
+      Repository buildRepo(Path basedir) throws IOException {
+        throw new IOException();
+      }
+    };
+    assertThat(provider.branchChangedFiles("branch", worktree)).isNull();
+  }
+
+  @Test
+  public void branchChangedFiles_should_return_null_on_io_errors_of_repo_exactref() throws IOException {
+    GitScmProvider provider = new GitScmProvider(mockCommand()) {
+      @Override
+      Repository buildRepo(Path basedir) throws IOException {
+        return mock(Repository.class);
+      }
+    };
+    assertThat(provider.branchChangedFiles("branch", worktree)).isNull();
+  }
+
+  @Test
+  public void branchChangedFiles_should_return_null_on_io_errors_of_RevWalk() throws IOException {
+    RevWalk walk = mock(RevWalk.class);
+    when(walk.parseCommit(any())).thenThrow(new IOException());
+
+    GitScmProvider provider = new GitScmProvider(mockCommand()) {
+      @Override
+      RevWalk newRevWalk(Repository repo) {
+        return walk;
+      }
+    };
+    assertThat(provider.branchChangedFiles("branch", worktree)).isNull();
+  }
+
+  @Test
+  public void branchChangedFiles_should_return_null_on_git_api_errors() throws GitAPIException {
+    DiffCommand diffCommand = mock(DiffCommand.class);
+    when(diffCommand.setShowNameAndStatusOnly(anyBoolean())).thenReturn(diffCommand);
+    when(diffCommand.setOldTree(any())).thenReturn(diffCommand);
+    when(diffCommand.call()).thenThrow(mock(GitAPIException.class));
+
+    Git git = mock(Git.class);
+    when(git.diff()).thenReturn(diffCommand);
+
+    GitScmProvider provider = new GitScmProvider(mockCommand()) {
+      @Override
+      Git newGit(Repository repo) {
+        return git;
+      }
+    };
+    assertThat(provider.branchChangedFiles("branch", worktree)).isNull();
+  }
+
+  private String randomizedContent(String prefix) {
+    StringBuilder sb = new StringBuilder(prefix);
+    for (int i = 0; i < 4; i++) {
+      sb.append(' ');
+      for (int j = 0; j < prefix.length(); j++) {
+        sb.append((char)('a' + random.nextInt(26)));
+      }
+    }
+    return sb.append("\n").toString();
+  }
+
+  private void createAndCommitFile(Path worktree, String filename) throws IOException, GitAPIException {
+    Path newFile = worktree.resolve(filename);
+    Files.write(newFile, randomizedContent(filename).getBytes(), StandardOpenOption.CREATE_NEW);
+    commit(filename);
+  }
+
+  private void appendToAndCommitFile(Path worktree, String filename) throws IOException, GitAPIException {
+    Files.write(worktree.resolve(filename), randomizedContent(filename).getBytes(), StandardOpenOption.APPEND);
+    commit(filename);
+  }
+
+  private void deleteAndCommitFile(String filename) throws IOException, GitAPIException {
+    git.rm().addFilepattern(filename).call();
+    commit(filename);
+  }
+
+  private void commit(String filename) throws GitAPIException {
+    git.add().addFilepattern(filename).call();
+    git.commit().setAuthor("joe", "joe@example.com").setMessage(filename).call();
+  }
+
+  private GitScmProvider newScmProvider() {
+    return new GitScmProvider(mockCommand());
+  }
+}
